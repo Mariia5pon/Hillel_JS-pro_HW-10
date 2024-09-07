42102d4edf03d9eb6e7c88ade81b22b90708e798
mariia <mariia@mail.uni-paderborn.de>
2024-09-07 14:26:43 +0200

    compledet task 1

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function getResultOfTask1() {
+    let user = {
+        name: "Mariia",
+        age: 25,
+        location: "Paderborn",
+
+        displayInfo: function () {
+            console.log(`Name: ${this.name}`);
+            console.log(`Age: ${this.age}`);
+            console.log(`Place of residence: ${this.location}`);
+        }
+    };
+    user.displayInfo();
+}
+
+function getResultOfTask2() {
+
+
+
+}
