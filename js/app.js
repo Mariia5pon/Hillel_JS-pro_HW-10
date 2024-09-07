236848f1087dd2f5008ea84eaf0c1e1ef86eb833
mariia <mariia@mail.uni-paderborn.de>
2024-09-07 14:41:13 +0200

    compledet task 2

--- js/app.js
+++ js/app.js
@@ -14,7 +14,10 @@ function getResultOfTask1() {
 }
 
 function getResultOfTask2() {
-
-
-
+    let numbers = [1, 2, 4, 3,258, 9,963, 5, 6, 7, 8, 9, 10];
+    let evenNumbers = numbers.filter(function(number) {
+        return number % 2 === 0;
+    });
+    console.log(evenNumbers);
 }
+
